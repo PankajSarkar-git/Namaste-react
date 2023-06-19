42935b41afa746aec5ea2bb773d7d1c72c532e00
Pankaj <pankajsarkarweb456@gmail.com>
2023-06-19 13:28:40 +0530

    Episode - 1

---
+++ App.js
@@ -0,0 +1,12 @@
+// const hedding = React.createElement("h1", { id: 'hedding', xyz: "abcd" }, "Hello World form React !");
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(hedding);
+
+
+
+const parent = React.createElement("div", { id: 'parent', xyz: "abcd" }, [React.createElement("div", { id: 'chaild', xyz: "abcd" },[React.createElement("h1", {}, 'I am h1 tag' ) ,React.createElement("h2", {}, 'I am h2 tag' )] ),React.createElement("div", { id: 'chaild2', xyz: "abcd" }, [React.createElement("h1", {}, 'I am h1 tag' ) ,React.createElement("h2", {}, 'I am h2 tag' )])]);
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
